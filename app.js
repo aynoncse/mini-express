654a014f0e26f7c76ec630f539ab83caae81a6a0
aynoncse <aynoncse@gmail.com>
2026-03-11 20:46:55 +0600

    feat: implement basic routing system with app.get()

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ const createApp = require('./framework/application');
 
 const app = createApp();
 
+app.get('/', (req, res) => {
+  res.end('Home Page');
+});
+
+app.get('/about', (req, res) => {
+  res.end('About Page');
+});
+
+app.get('/users', (req, res) => {
+  res.end('Users Page');
+});
+
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
--- framework/application.js
+++ framework/application.js
@@ -2,21 +2,26 @@
  * Express-like application factory.
  * Creates an HTTP server.
  *
- * @module Router
+ * @module createApp
  * @author Aynon Bhuiyan <aynoncse@gmail.com>
  * @created 2026-03-11
  */
 
 const http = require('http');
+const Router = require('./router');
 
 function createApp() {
+  const router = new Router();
   function app(req, res) {
-    res.end('Server working');
+    router.handle(req, res);
   }
+
+  app.get = (path, handler) => {
+    router.register('GET', path, handler);
+  };
 
   app.listen = (port, callback) => {
     const server = http.createServer(app);
-
     server.listen(port, callback);
   };
 
